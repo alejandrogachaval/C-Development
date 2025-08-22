6ab8b23d237e08cfcfae4114647ba4fc8fc99786
Alejandro Gonzalez Achaval <alejandrogonzalezachaval@gmail.com>
2025-08-22 09:24:00 -0300

    Agregue el archivo '2-Variables.c'

---
+++ Introduccion/2-Variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Variables
+int main() {
+	//Declarar una variable 
+	int numero;
+	//La inicializamos
+	numero=5;
+	//Imprimimos la variable
+	//Formato %d significa que es decimal o entero
+	printf("%d", numero);
+	return 0;
+}
+
