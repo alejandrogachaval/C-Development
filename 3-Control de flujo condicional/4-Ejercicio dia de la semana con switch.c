3feeef224740d7769b82bdd9c926cd24b0899d15
Alejandro Gonzalez Achaval <alejandrogonzalezachaval@gmail.com>
2025-09-16 20:53:40 -0300

    Agrego ejercicio día de la semana con switch

---
+++ 3-Control de flujo condicional/4-Ejercicio dia de la semana con switch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	int dia;
+	
+	printf("Ingrese el dia de la semana (1-7): ");
+	scanf("%d", &dia);
+	
+	switch(dia){
+		case 1:
+			printf("Lunes");
+			break;
+		case 2:
+			printf("Martes");
+			break;
+		case 3:
+			printf("Miercoles");
+			break;
+		case 4:
+			printf("Jueves");
+			break;
+		case 5:
+			printf("Viernes");
+			break;
+		case 6:
+			printf("Sabado");
+			break;
+		case 7:
+			printf("Domingo");
+			break;
+		default:
+			printf("No ha ingresado un valor valido");
+	}
+	return 0;
+}
+
