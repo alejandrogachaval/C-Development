afe5f2d75cd200248a54047630974677a53a6b59
Alejandro Gonzalez Achaval <alejandrogonzalezachaval@gmail.com>
2025-09-17 20:17:17 -0300

    Correccion de ejercicio de ciclo for con salto de contador

--- 4-Ciclos/4-Ejercicio ciclo for salto contador.c
+++ 4-Ciclos/4-Ejercicio ciclo for salto contador.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 //Incrementar de 3 en 3 con tope de 10 y restar 3 en 3 con tope -10
 int main() {
-	const int MAXIMO = 10;
+	const int TOPE = 10;
 	printf("Saltos de suma:\n");
-	for(int i=0; i <= MAXIMO; i+=3){
+	for(int i=0; i <= TOPE; i+=3){
 		printf("%d\n", i);
 	}
 	printf("\nSaltos de resta:\n");
-	for(int i=0; i >= -MAXIMO; i-=3){
+	for(int i=0; i >= -TOPE; i-=3){
 		printf("%d\n", i);
 	}
 	return 0;
