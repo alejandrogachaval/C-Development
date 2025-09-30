646c3931820200185181ef0da3d29e3b887b896c
Alejandro Gonzalez Achaval <alejandrogonzalezachaval@gmail.com>
2025-09-30 20:22:37 -0300

    Interacion de arreglos

---
+++ 5-Arreglos/3-Iteracion de arreglos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int arregloNumerico[] = {72,122,33,27,1245};
+	
+	
+	//Iterar el arreglo
+	for(int i=0; i < 5; i++){
+		printf("\nElemento del arregloNumerico[%d]: %d", i, arregloNumerico[i]);
+	}
+	return 0;
+}
+
